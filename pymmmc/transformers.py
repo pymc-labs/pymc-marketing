326446bba6e8ccd30ceca7d260d0c6c9532b2c31
juanitorduz <juan.orduz@wolt.com>
2022-03-29 21:14:35 +0200

    remove parameters conditions and tests

--- pymmmc/transformers.py
+++ pymmmc/transformers.py
@@ -31,9 +31,6 @@ def geometric_adstock(x, alpha: float = 0.0, l_max: int = 12, normalize: bool = 
     .. [1] Jin, Yuxue, et al. "Bayesian methods for media mix modeling
     with carryover and shape effects." (2017).
     """
-    if not 0 < alpha < 1:
-        raise ValueError(f"alpha must be between 0 and 1. Got {alpha}")
-
     cycles = [at.concatenate([at.zeros(i), x[: x.shape[0] - i]]) for i in range(l_max)]
     x_cycle = at.stack(cycles)
     w = at.as_tensor_variable([at.power(alpha, i) for i in range(l_max)])
@@ -72,11 +69,6 @@ def delayed_adstock(
     .. [1] Jin, Yuxue, et al. "Bayesian methods for media mix modeling
     with carryover and shape effects." (2017).
     """
-    if not 0 < alpha < 1:
-        raise ValueError(f"alpha must be between 0 and 1. Got {alpha}")
-    if not 0 <= theta < l_max:
-        raise ValueError(f"theta must be between 0 and l_max - 1. Got {theta}")
-
     cycles = [at.concatenate([at.zeros(i), x[: x.shape[0] - i]]) for i in range(l_max)]
     x_cycle = at.stack(cycles)
     w = at.as_tensor_variable(
@@ -101,8 +93,6 @@ def logistic_saturation(x, lam: float = 0.5):
     tensor
         Transformed tensor.
     """
-    if lam < 0:
-        raise ValueError(f"lam must be non-negative. Got {lam}")
     return (1 - at.exp(-lam * x)) / (1 + at.exp(-lam * x))
 
 
@@ -127,8 +117,4 @@ def tanh_saturation(x, b: float = 0.5, c: float = 0.5):
     ----------
     See https://www.pymc-labs.io/blog-posts/reducing-customer-acquisition-costs-how-we-helped-optimizing-hellofreshs-marketing-budget/ # noqa: E501
     """
-    if b < 0:
-        raise ValueError(f"b must be non-negative. Got {b}")
-    if c == 0:
-        raise ValueError("c must be non-zero.")
     return b * at.tanh(x / (b * c))
