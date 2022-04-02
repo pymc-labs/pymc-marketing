fc64a9fee569f6f528152b4292e1975753c8c425
juanitorduz <juan.orduz@wolt.com>
2022-04-02 13:04:05 +0200

    fix lint

--- pymmmc/tests/test_transformers.py
+++ pymmmc/tests/test_transformers.py
@@ -57,7 +57,7 @@ def test_geometric_adstock_good_alpha(x, alpha, l_max):
     y_np = y.eval()
     assert y_np[0] == x[0]
     assert y_np[1] == x[1] + alpha * x[0]
-    assert y_np[2] == x[2] + alpha * x[1] + (alpha ** 2) * x[0]
+    assert y_np[2] == x[2] + alpha * x[1] + (alpha**2) * x[0]
 
 
 def test_delayed_adstock_output_type():
