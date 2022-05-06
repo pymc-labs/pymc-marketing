49cc80bf6b3b29325ffa17113612a3d4b8d3b354
Benjamin T. Vincent <b.t.vincent@dundee.ac.uk>
2022-05-06 16:00:16 +0100

    adds a plotting utility function - median + hdi's

---
+++ notebooks/plot_utils.py
@@ -0,0 +1,32 @@
+import arviz as az
+import matplotlib.pyplot as plt
+
+
+def plot_xY(x, Y, ax=None):
+    """Plot the posterior mean and 95% and 50% CI's from a given set of x and Y values.
+    x: is a vector
+    Y: is an xarrray of size (chain, draw, dim)
+    """
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    quantiles = Y.quantile(
+        (0.025, 0.25, 0.5, 0.75, 0.975), dim=("chain", "draw")
+    ).transpose()
+
+    az.plot_hdi(
+        x,
+        hdi_data=quantiles.sel(quantile=[0.025, 0.975]),
+        ax=ax,
+        fill_kwargs={"label": "95% ETI"},
+    )
+    az.plot_hdi(
+        x,
+        hdi_data=quantiles.sel(quantile=[0.25, 0.75]),
+        ax=ax,
+        fill_kwargs={"label": "50% ETI"},
+    )
+    ax.plot(x, quantiles.sel(quantile=0.5), label="Median")
+    ax.legend()
+
+    return fig, ax
