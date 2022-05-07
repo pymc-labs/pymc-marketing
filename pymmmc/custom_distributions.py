d1288177aadd22fb4369c98abe8d9614b56db604
Benjamin T. Vincent <b.t.vincent@dundee.ac.uk>
2022-05-07 14:47:35 +0100

    failed attempt to fix likelihood

--- pymmmc/custom_distributions.py
+++ pymmmc/custom_distributions.py
@@ -21,11 +21,33 @@ def truncated_geometric_logp(theta, customers):
         Vector of number of customers. Should be non-increasing.
     """
     churned_in_period_t = customers[:-1] - customers[1:]
-    T = len(customers)
-    time_periods = np.arange(start=1, stop=T)
+
+    # NOTE: T is appropriate for indexing into the last element of `customers`
+    # But when we include T in the likelihood, we need to +1 because of zero based indexing
+    nT = customers[-1]
+    T = len(customers) + 1
+
+    # We can't learn anything about theta if we only have one observation (equal to the initial cohort size)
+    assert T > 1
+
+    t_vec = np.arange(start=1, stop=len(customers))
+
     logp = 0
+
+    # likelihood for all non-truncated time steps [1,... T-1]
     logp += at.math.sum(
-        churned_in_period_t * (at.log(theta) + ((time_periods - 1) * at.log(1 - theta)))
+        churned_in_period_t * (at.log(theta) + ((t_vec - 1) * at.log(1 - theta)))
     )
-    logp += customers[T - 1] * (T * at.log(1 - theta))
+
+    # likelihood for final time step T
+    logp += nT * T * at.log(1 - theta)
+
+    # # likelihood for all non-truncated time steps [1,... T-1]
+    # logp += at.math.sum(
+    #     churned_in_period_t * (at.log(theta * at.pow((1 - theta), t_vec - 1)))
+    # )
+
+    # # likelihood for final time step T
+    # logp += nT * at.log(at.pow(1 - theta, T))
+
     return logp
