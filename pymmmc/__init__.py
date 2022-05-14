52bee3e08350c492b17a1af8053601aa75fb4534
Larry Dong <larry.dong@mail.utoronto.ca>
2022-05-14 14:46:49 -0400

    Adding continuous contractual distribution + notebook

--- pymmmc/__init__.py
+++ pymmmc/__init__.py
@@ -1,7 +1,8 @@
 __version__ = "0.0.1"
 
-from pymmmc.distributions import ContNonContract
+from pymmmc.distributions import ContinuousContractual, ContNonContract
 
 __all__ = [
     "ContNonContract",
+    "ContContract",
 ]
--- pymmmc/distributions.py
+++ pymmmc/distributions.py
@@ -143,3 +143,126 @@ class ContNonContract(PositiveContinuous):
             at.all(T0 < T),
             msg="lam > 0, 0 <= p <= 1, T0 < T",
         )
+
+
+class ContContractRV(RandomVariable):
+    name = "continuous_contractual"
+    ndim_supp = 1
+    ndims_params = [0, 0, 0, 0]
+    dtype = "floatX"
+    _print_name = ("ContinuousContractual", "\\operatorname{ContinuousContractual}")
+
+    def make_node(self, rng, size, dtype, lam, p, T, T0):
+
+        T = at.as_tensor_variable(T)
+        T0 = at.as_tensor_variable(T0)
+
+        return super().make_node(rng, size, dtype, lam, p, T, T0)
+
+    def __call__(self, lam, p, T, T0=0, size=None, **kwargs):
+        return super().__call__(lam, p, T, T0, size=size, **kwargs)
+
+    @classmethod
+    def rng_fn(cls, rng, lam, p, T, T0, size) -> np.array:
+
+        size = pm.distributions.shape_utils.to_tuple(size)
+
+        # To do: broadcast sizes
+        lam = np.asarray(lam)
+        p = np.asarray(p)
+        T = np.asarray(T)
+        T0 = np.asarray(T0)
+
+        param_shape = np.broadcast_shapes(lam.shape, p.shape, T.shape, T0.shape)
+        size = param_shape + size
+
+        lam = np.broadcast_to(lam, size)
+        p = np.broadcast_to(p, size)
+        T = np.broadcast_to(T, size)
+        T0 = np.broadcast_to(T0, size)
+
+        output = np.zeros(shape=size + (3,))
+
+        def sim_data(lam, p, T, T0):
+            t = 0
+            n = 0
+
+            while True:
+                wait = rng.exponential(scale=1 / lam)
+                dropout = rng.binomial(n=1, p=p)
+
+                if t + wait > T:
+                    break
+                else:
+                    t += wait
+                    n += 1
+
+                    if dropout == 1:
+                        break
+
+            return np.array(
+                [
+                    t,
+                    n,
+                    dropout,
+                ],
+            )
+
+        for index in np.ndindex(*size):
+            output[index] = sim_data(lam[index], p[index], T[index], T0[index])
+
+        return output
+
+    def _supp_shape_from_params(*args, **kwargs):
+        return (3,)
+
+
+continuous_contractual = ContContractRV()
+
+
+class ContContract(PositiveContinuous):
+    rv_op = continuous_contractual
+
+    @classmethod
+    def dist(cls, lam, p, T, T0, **kwargs):
+        return super().dist([lam, p, T, T0], **kwargs)
+
+    def logp(value, lam, p, T, T0):
+        t_x = value[..., 0]
+        x = value[..., 1]
+        churn = value[..., 2]
+
+        zero_observations = at.eq(x, 0)
+
+        logp = (x - 1) * at.log(1 - p) + x * at.log(lam) - lam * t_x
+        logp += churn * at.log(p) + (1 - churn) * (at.log(1 - p) - lam * (T - t_x))
+
+        logp = at.switch(
+            zero_observations,
+            -lam * (T - T0),
+            logp,
+        )
+
+        logp = at.sum(logp, axis=-1)
+
+        logp = at.switch(
+            at.any(at.or_(at.lt(t_x, 0), at.lt(x, 0))),
+            -np.inf,
+            logp,
+        )
+        logp = at.switch(
+            at.all(
+                at.or_(at.eq(churn, 0), at.eq(churn, 1)),
+            ),
+            -np.inf,
+            logp,
+        )
+
+        return check_parameters(
+            logp,
+            lam > 0,
+            0 <= p,
+            p <= 1,
+            at.all(T0 < T),
+            msg="lam > 0, 0 <= p <= 1, T0 < T",
+        )
