ccf38f673a98dfb8a36651eb71e12210913d694d
juanitorduz <juan.orduz@wolt.com>
2022-07-20 11:05:05 +0200

    scan implementation od adstock trasnformations

--- pymmmc/transformers.py
+++ pymmmc/transformers.py
@@ -1,3 +1,4 @@
+import aesara
 import aesara.tensor as at
 
 
@@ -8,7 +9,6 @@ def geometric_adstock(x, alpha: float = 0.0, l_max: int = 12, normalize: bool = 
     time period as ad exposure. The cumulative media effect is a weighted average
     of media spend in the current time-period (e.g. week) and previous `l_max` - 1
     periods (e.g. weeks). `l_max` is the maximum duration of carryover effect.
-
 
     Parameters
     ----------
@@ -31,8 +31,12 @@ def geometric_adstock(x, alpha: float = 0.0, l_max: int = 12, normalize: bool = 
     .. [1] Jin, Yuxue, et al. "Bayesian methods for media mix modeling
     with carryover and shape effects." (2017).
     """
-    cycles = [at.concatenate([at.zeros(i), x[: x.shape[0] - i]]) for i in range(l_max)]
-    x_cycle = at.stack(cycles)
+    x_cycle, _ = aesara.scan(
+        fn=lambda i, _, x: at.concatenate([at.zeros(i), x[: x.shape[0] - i]]),
+        sequences=at.arange(l_max),
+        outputs_info=at.zeros_like(x),
+        non_sequences=x,
+    )
     w = at.as_tensor_variable([at.power(alpha, i) for i in range(l_max)])
     w = w / at.sum(w) if normalize else w
     return at.dot(w, x_cycle)
@@ -40,11 +44,14 @@ def geometric_adstock(x, alpha: float = 0.0, l_max: int = 12, normalize: bool = 
 
 def geometric_adstock_vectorized(x, alpha, l_max: int = 12, normalize: bool = False):
     """Vectorized geometric adstock transformation."""
-    cycles = [
-        at.concatenate(tensor_list=[at.zeros(shape=x.shape)[:i], x[: x.shape[0] - i]])
-        for i in range(l_max)
-    ]
-    x_cycle = at.stack(cycles)
+    x_cycle, _ = aesara.scan(
+        fn=lambda i, _, x: at.concatenate(
+            tensor_list=[at.zeros(shape=x.shape)[:i], x[: x.shape[0] - i]]
+        ),
+        sequences=at.arange(l_max),
+        outputs_info=at.zeros_like(x),
+        non_sequences=x,
+    )
     x_cycle = at.transpose(x=x_cycle, axes=[1, 2, 0])
     w = at.as_tensor_variable([at.power(alpha, i) for i in range(l_max)])
     w = at.transpose(w)[None, ...]
@@ -65,7 +72,7 @@ def delayed_adstock(
     x : tensor
         Input tensor.
     alpha : float, by default 0.0
-         Retention rate of ad effect. Must be between 0 and 1.
+        Retention rate of ad effect. Must be between 0 and 1.
     theta : float, by default 0
         Delay of the peak effect. Must be between 0 and `l_max` - 1.
     l_max : int, by default 12
@@ -83,8 +90,12 @@ def delayed_adstock(
     .. [1] Jin, Yuxue, et al. "Bayesian methods for media mix modeling
     with carryover and shape effects." (2017).
     """
-    cycles = [at.concatenate([at.zeros(i), x[: x.shape[0] - i]]) for i in range(l_max)]
-    x_cycle = at.stack(cycles)
+    x_cycle, _ = aesara.scan(
+        fn=lambda i, _, x: at.concatenate([at.zeros(i), x[: x.shape[0] - i]]),
+        sequences=at.arange(l_max),
+        outputs_info=at.zeros_like(x),
+        non_sequences=x,
+    )
     w = at.as_tensor_variable(
         [at.power(alpha, ((i - theta) ** 2)) for i in range(l_max)]
     )
@@ -96,11 +107,14 @@ def delayed_adstock_vectorized(
     x, alpha, theta, l_max: int = 12, normalize: bool = False
 ):
     """Delayed adstock transformation."""
-    cycles = [
-        at.concatenate(tensor_list=[at.zeros(shape=x.shape)[:i], x[: x.shape[0] - i]])
-        for i in range(l_max)
-    ]
-    x_cycle = at.stack(cycles)
+    x_cycle, _ = aesara.scan(
+        fn=lambda i, _, x: at.concatenate(
+            tensor_list=[at.zeros(shape=x.shape)[:i], x[: x.shape[0] - i]]
+        ),
+        sequences=at.arange(l_max),
+        outputs_info=at.zeros_like(x),
+        non_sequences=x,
+    )
     x_cycle = at.transpose(x=x_cycle, axes=[1, 2, 0])
     w = at.as_tensor_variable(
         [at.power(alpha, ((i - theta) ** 2)) for i in range(l_max)]
