fe114062dae21362f469560130ae78465f48a84f
lucianopaz <luciano.paz.neuro@gmail.com>
2022-11-21 12:05:32 +0100

    Refactor MMM into Base and subclass. Mark validation and preprocess methods with decorators

---
+++ pymmmc/mmm/delayed_saturated_mmm.py
@@ -0,0 +1,129 @@
+from typing import Any, Dict, List, Optional
+
+import pandas as pd
+import pymc as pm
+
+from pymmmc.mmm.base import RescaledMMM, validation_method
+from pymmmc.transformers import geometric_adstock_vectorized, logistic_saturation
+
+
+class DelayedSaturatedMMM(RescaledMMM):
+    def __init__(
+        self,
+        data_df: pd.DataFrame,
+        target_column: str,
+        date_column: str,
+        channel_columns: List[str],
+        validate_data: bool = True,
+        control_columns: Optional[List[str]] = None,
+        adstock_max_lag: int = 4,
+        **kwargs,
+    ) -> None:
+        self.control_columns = control_columns
+        self.adstock_max_lag = adstock_max_lag
+        super().__init__(
+            data_df=data_df,
+            target_column=target_column,
+            date_column=date_column,
+            channel_columns=channel_columns,
+            validate_data=validate_data,
+            adstock_max_lag=adstock_max_lag,
+        )
+
+    def build_model(
+        self,
+        data_df: pd.DataFrame,
+        adstock_max_lag: int = 4,
+    ) -> None:
+        date_data = data_df[self.date_column]
+        target_data = data_df[self.target_column]
+        channel_data = data_df[self.channel_columns]
+        if self.control_columns is not None:
+            control_data: Optional[pd.DataFrame] = data_df[self.control_columns]
+        else:
+            control_data = None
+        coords: Dict[str, Any] = {
+            "date": date_data,
+            "channel": channel_data.columns,
+        }
+
+        if control_data is not None:
+            coords["control_names"] = control_data.columns
+
+        with pm.Model(coords=coords) as self.model:
+            channel_data_ = pm.MutableData(
+                name="channel_data",
+                value=channel_data,
+                dims=("date", "channel"),
+            )
+
+            target_ = pm.MutableData(name="target", value=target_data, dims="date")
+
+            intercept = pm.Normal(name="intercept", mu=0, sigma=2)
+
+            beta_channel = pm.HalfNormal(
+                name="beta_channel", sigma=2, dims="channel"
+            )  # ? Allow prior depend on channel costs?
+
+            alpha = pm.Beta(name="alpha", alpha=1, beta=3, dims="channel")
+
+            lam = pm.Gamma(name="lam", alpha=3, beta=1, dims="channel")
+
+            sigma = pm.HalfNormal(name="sigma", sigma=2)
+
+            channel_adstock = pm.Deterministic(
+                name="channel_adstock",
+                var=geometric_adstock_vectorized(
+                    x=channel_data_,
+                    alpha=alpha,
+                    l_max=adstock_max_lag,
+                    normalize=True,
+                ),
+                dims=("date", "channel"),
+            )
+            channel_adstock_saturated = pm.Deterministic(
+                name="channel_adstock_saturated",
+                var=logistic_saturation(x=channel_adstock, lam=lam),
+                dims=("date", "channel"),
+            )
+            channel_contribution = pm.Deterministic(
+                name="channel_contribution",
+                var=pm.math.dot(channel_adstock_saturated, beta_channel),
+                dims="date",
+            )
+
+            mu_var = intercept + channel_contribution
+
+            if control_data is not None:
+                control_data_ = pm.MutableData(
+                    name="control_data", value=control_data, dims=("date", "control")
+                )
+
+                gamma_control = pm.Normal(
+                    name="gamma_control", mu=0, sigma=2, dims="control"
+                )
+
+                control_contribution = pm.Deterministic(
+                    name="control_contribution",
+                    var=pm.math.dot(control_data_, gamma_control),
+                    dims="date",
+                )
+
+                mu_var += control_contribution
+
+            mu = pm.Deterministic(name="mu", var=mu_var, dims="date")
+
+            pm.Normal(
+                name="likelihood",
+                mu=mu,
+                sigma=sigma,
+                observed=target_,
+                dims="date",
+            )
+
+    @validation_method
+    def _validate_control_columns(self, data_df) -> None:
+        if self.control_columns is not None and not set(self.control_columns).issubset(
+            data_df.columns
+        ):
+            raise ValueError(f"control_columns {self.control_columns} not in data_df")
