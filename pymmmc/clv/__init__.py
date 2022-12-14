7cb61eb22686fdcd6ab8a29649f37aef6253d313
larryshamalama <larry.dong@mail.utoronto.ca>
2022-12-14 09:06:40 -0500

    Adding BetaGeoModel to clv submodule (#67)
    * Adding BetaGeoModel to clv submodule
    
    * Adding tests for BetaGeoModel
    
    * Adding notebooks related to BetaGeoModel

--- pymmmc/clv/__init__.py
+++ pymmmc/clv/__init__.py
@@ -1,8 +1,10 @@
 from pymmmc.clv import models
 from pymmmc.clv.models.basic import CLVModel
+from pymmmc.clv.models.beta_geo import BetaGeoModel
 from pymmmc.clv.models.gamma_gamma import GammaGammaModel, GammaGammaModelIndividual
 
 __all__ = (
+    "BetaGeoModel",
     "CLVModel",
     "GammaGammaModel",
     "GammaGammaModelIndividual",
