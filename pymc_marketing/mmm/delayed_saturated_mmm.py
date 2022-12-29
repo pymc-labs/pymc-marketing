20580cb05f17674360fa8635ad82cea890339e90
juanitorduz <juanitorduz@gmail.com>
2022-12-29 22:22:28 +0100

    correct contribution original scale

--- pymc_marketing/mmm/delayed_saturated_mmm.py
+++ pymc_marketing/mmm/delayed_saturated_mmm.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict, List, Optional
 
+import arviz as az
 import pandas as pd
 import pymc as pm
+from xarray import DataArray
 
 from pymc_marketing.mmm.base import MMM
 from pymc_marketing.mmm.preprocessing import MaxAbsScaleChannels, MixMaxScaleTarget
@@ -127,3 +129,21 @@ class DelayedSaturatedMMM(
                 observed=target_,
                 dims="date",
             )
+
+    def compute_channel_contribution_original_scale(self) -> DataArray:
+        beta_channel_samples_extended: DataArray = az.extract(
+            data=self.fit_result, var_names=["beta_channel"], combined=False
+        ).expand_dims({"date": self.n_obs}, axis=2)
+
+        channel_transformed: DataArray = az.extract(
+            data=self.fit_result,
+            var_names=["channel_adstock_saturated"],
+            combined=False,
+        )
+
+        normalization_factor: float = self.target_transformer.named_steps[
+            "scaler"
+        ].scale_.item()
+        return (
+            beta_channel_samples_extended * channel_transformed
+        ) / normalization_factor
