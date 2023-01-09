bb6d8fa79fa6ec75ab46815c4a7f34b037757a0c
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2023-01-09 09:14:17 +0100

    Reorganize CLV imports

--- pymc_marketing/clv/models/__init__.py
+++ pymc_marketing/clv/models/__init__.py
@@ -1,1 +1,13 @@
-from pymc_marketing.clv.models import basic, gamma_gamma
+from pymc_marketing.clv.models.basic import CLVModel
+from pymc_marketing.clv.models.beta_geo import BetaGeoModel
+from pymc_marketing.clv.models.gamma_gamma import (
+    GammaGammaModel,
+    GammaGammaModelIndividual,
+)
+
+__all__ = (
+    "CLVModel",
+    "GammaGammaModel",
+    "GammaGammaModelIndividual",
+    "BetaGeoModel",
+)
