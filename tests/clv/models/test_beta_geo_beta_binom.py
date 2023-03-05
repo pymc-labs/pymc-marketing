82cb3af86bbf89fed4cb9def47137fd0b87ef7fd
ColtAllen <colt.allen1@gmail.com>
2023-03-05 12:04:40 -0700

    WIP test framework for BGBB

--- tests/clv/models/test_beta_geo_beta_binom.py
+++ tests/clv/models/test_beta_geo_beta_binom.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pymc as pm
 import pytest
-from lifetimes import BetaGeoFitter as BGF
+from lifetimes import BetaGeoBetaBinomFitter
 
 from pymc_marketing.clv.models.beta_geo_beta_binom import BetaGeoBetaBinomModel
 
@@ -35,73 +35,76 @@ def donations() -> pd.DataFrame:
 
 @pytest.mark.skip(reason="Still a WIP")
 class TestBetaGeoBetaBinomModel:
-    # Hyperparameters
-    alpha_true = 0.8
-    beta_true = 2.5
-    gamma_true = 3
-    delta_true = 4
-    rng = np.random.default_rng(34)
-
-    N = 2000
-
     @classmethod
-    def setup_class(cls):
-        cls.customer_id = list(range(cls.N))
-        cls.recency, cls.frequency, cls.alive, cls.T = cls.generate_data(cls.N)
+    def setup_class(cls, donations):
+        # Set random seed
+        rng = np.random.default_rng(34)
 
-        # fit the model once for some tests
-        cls.fixed_model = BetaGeoBetaBinomModel(
+        # Parameters
+        alpha_true = 1.204
+        beta_true = 0.750
+        gamma_true = 0.657
+        delta_true = 2.783
+
+        # Define testing data
+        cls.customer_id = donations.index
+        cls.recency = donations["recency"]
+        cls.frequency = donations["frequency"]
+        cls.T = donations["T"]
+
+        # Initialize model for testing
+        cls.model = BetaGeoBetaBinomModel(
             customer_id=cls.customer_id,
             frequency=cls.frequency,
             recency=cls.recency,
             T=cls.T,
         )
-        cls.fixed_model.fit(chains=1, progressbar=False, random_seed=cls.rng)
 
-        cls.test_t = np.linspace(20, 38, 10)
-        cls.test_frequency = np.tile([1, 3, 5, 7, 9], 2)
-        cls.test_recency = np.tile([20, 30], 5)
-        cls.test_T = np.tile([25, 35], 5)
+        # Instead of fitting BetaGeoBetaBinomFitter,
+        # Load the true model parameters directly
+        BBF._unload_params = (
+            cls.alpha_true,
+            cls.beta_true,
+            cls.gamma_true,
+            cls.delta_true,
+        )
 
-        def overwrite_bgf_unload_params(self, *args, **kwargs):
-            """
-            The methods from BetaGeoFitter rely on a fitted model, i.e. estimates
-            for a, b, alpha and r. This function circumvents the need to use a
-            fitted model and uses the data-generating parameters for this test
-            instead.
-            """
-            return cls.r_true, cls.alpha_true, cls.a_true, cls.b_true
-
-        BGF._unload_params = overwrite_bgf_unload_params
-
+        # TODO: Is this declaration needed?
         BGF.conditional_expected_number_of_purchases_up_to_time = classmethod(
             BGF.conditional_expected_number_of_purchases_up_to_time
         )
 
-        cls.expected_test_num_purchases = (
+        cls.test_num_purchases = (
             BGF.conditional_expected_number_of_purchases_up_to_time(
-                t=cls.test_t,
-                frequency=cls.test_frequency,
-                recency=cls.test_recency,
-                T=cls.test_T,
+                m_periods_in_future=cls.test_t,
+                frequency=cls.frequency,
+                recency=cls.recency,
+                n_periods=cls.T,
             )
         )
 
-        BGF.expected_number_of_purchases_up_to_time = classmethod(
-            BGF.expected_number_of_purchases_up_to_time
+        # TODO: Is this declaration needed?
+        BGF.conditional_probability_alive = classmethod(
+            BGF.conditional_probability_alive
         )
 
-        cls.expected_test_num_purchases_new_customer = (
-            BGF.expected_number_of_purchases_up_to_time(t=cls.test_t)
+        cls.test_alive = BGF.conditional_probability_alive(
+            m_periods_in_future=cls.test_t,
+            frequency=cls.frequency,
+            recency=cls.recency,
+            n_periods=cls.T,
         )
 
     def test_unload_params(self):
         pass
 
     @pytest.mark.parametrize(
-        "purchase_hyperprior", (None, pm.Pareto.dist(alpha=1, m=1.5))
+        "purchase_hyperprior, churn_hyperprior",
+        [
+            (None, None),
+            (pm.Pareto.dist(alpha=1, m=1.5), pm.Pareto.dist(alpha=1, m=1.5)),
+        ],
     )
-    @pytest.mark.parametrize("churn_hyperprior", (None, pm.Pareto.dist(alpha=1, m=1.5)))
     def test_model(self, purchase_hyperprior, churn_hyperprior):
         model = BetaGeoBetaBinomModel(
             customer_id=self.customer_id,
@@ -114,11 +117,11 @@ class TestBetaGeoBetaBinomModel:
 
         assert isinstance(
             model.model["purchase_hyperprior"].owner.op,
-            pm.Pareto if a_prior is None else type(purchase_hyperprior.owner.op),
+            pm.HalfFlat if a_prior is None else type(purchase_hyperprior.owner.op),
         )
         assert isinstance(
             model.model["churn_hyperprior"].owner.op,
-            pm.Pareto if b_prior is None else type(churn_hyperprior.owner.op),
+            pm.HalfFlat if b_prior is None else type(churn_hyperprior.owner.op),
         )
         assert model.model.eval_rv_shapes() == {
             "purchase_hyperprior": (),
@@ -143,81 +146,75 @@ class TestBetaGeoBetaBinomModel:
             "churn_heterogeneity_log__": (),
         }
 
+    @pytest.mark.skip(reason="logp tests failing")
+    @pytest.mark.slow
     @pytest.mark.parametrize(
-        "N, rtol",
+        "fit_method, rtol",
         [
-            (500, 0.3),
-            (2000, 0.1),
-            (10000, 0.055),
+            ("mcmc", 0.1),
+            ("map", 0.3),
         ],
     )
-    def test_model_convergence(self, donations):
-        recency, frequency, _, T = self.generate_data(N)
-
-        # b parameter has the largest mismatch of the four parameters
-        model = BetaGeoBetaBinomModel(
-            customer_id=list(range(len(frequency))),
-            frequency=frequency,
-            recency=recency,
-            T=T,
+    def test_model_convergence(self, fit_method, rtol):
+        self.model.fit(
+            fitting_method=fit_method, chains=1, progressbar=False, random_seed=cls.rng
         )
-        model.fit(chains=1, progressbar=False, random_seed=self.rng)
-        fit = model.fit_result.posterior
+
+        fit = self.model.fit_result.posterior
         np.testing.assert_allclose(
-            [fit["a"].mean(), fit["b"].mean(), fit["alpha"].mean(), fit["r"].mean()],
-            [self.a_true, self.b_true, self.alpha_true, self.r_true],
+            [
+                fit["alpha"].mean(),
+                fit["beta"].mean(),
+                fit["gamma"].mean(),
+                fit["delta"].mean(),
+            ],
+            [self.alpha_true, self.beta_true, self.gamma_true, self.delta_true],
             rtol=rtol,
         )
 
-    def test_expected_probability_alive(self):
+    @pytest.mark.skip(reason="logp tests failing")
+    def test_probability_alive(self):
         """
         The "true" prefix refers to the value obtained using 1) the closed form
         solution and 2) the data-generating parameter values.
         """
-        true_prob_alive = self.alive.mean()  # scalar
-        est_prob_alive = self.fixed_model.expected_probability_alive(
-            self.customer_id,
-            self.frequency,
-            self.recency,
-            self.T,
-        )
+        true_prob_alive = self.test_alive.mean()
 
-        assert est_prob_alive.shape == (1, 1000, self.N)
+        est_prob_alive = self.model.probability_alive(t=self.test_t)
+
+        assert est_prob_alive.shape == (1, 1000, len(self.customer_id))
         assert est_prob_alive.dims == ("chain", "draw", "customer_id")
 
         np.testing.assert_allclose(
-            true_prob_alive.mean(),
+            true_prob_alive,
             est_prob_alive.mean(),
             rtol=0.05,
         )
 
-    def test_expected_num_purchases(self):
-        """
-        TODO: should we combine this test and the one below?
-        """
-        est_num_purchases = self.fixed_model.expected_num_purchases(
-            list(range(20, 40, 2)),
-            self.test_t,
-            self.test_frequency,
-            self.test_recency,
-            self.test_T,
-        )
+    @pytest.mark.skip(reason="logp tests failing")
+    def test_expected_purchases(self):
+        true_prob_alive = self.test_num_purchases.mean()
 
-        assert est_num_purchases.shape == (1, 1000, 10)
+        est_purchases = self.fixed_model.expected_purchases(self.test_t, self.test_x)
+
+        assert est_num_purchases.shape == (1, 1000, len(self.customer_id))
         assert est_num_purchases.dims == ("chain", "draw", "customer_id")
 
         np.testing.assert_allclose(
-            self.expected_test_num_purchases,
-            est_num_purchases.mean(("chain", "draw")),
+            self.test_num_purchases,
+            est_purchases.mean(("chain", "draw")),
             rtol=0.1,
         )
 
-    def test_expected_num_purchases_new_customer(self):
-        est_num_purchases = self.fixed_model.expected_num_purchases_new_customer(
+    @pytest.mark.skip(reason="logp tests failing")
+    def test_expected_purchases_new_customer(self):
+        # TODO: Will need to hardcode expected values from http://brucehardie.com/notes/010/
+
+        est_num_purchases = self.fixed_model.expected_purchases_new_customer(
             self.test_t
         )
 
-        assert est_num_purchases.shape == (1, 1000, 10)
+        assert est_num_purchases.shape == (1, 1000, 1)
         assert est_num_purchases.dims == ("chain", "draw", "t")
 
         np.testing.assert_allclose(
