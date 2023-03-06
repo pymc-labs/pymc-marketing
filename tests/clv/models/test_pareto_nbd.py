e4b164a7785d6561270e1d67f6165a0a0a6bd15f
ColtAllen <colt.allen1@gmail.com>
2023-03-05 19:16:11 -0700

    WIP test framework

--- tests/clv/models/test_pareto_nbd.py
+++ tests/clv/models/test_pareto_nbd.py
@@ -1,65 +1,77 @@
 import numpy as np
 import pymc as pm
 import pytest
-from lifetimes import ParetoNBDFitter as PNF
+from lifetimes import ParetoNBDFitter
 
-from pymc_marketing.clv.distributions import ParetoNBD
 from pymc_marketing.clv.models.pareto_nbd import ParetoNBDModel
 
 
-@pytest.mark.skip(reason="Still a WIP")
 class TestParetoNBDModel:
-    # Parameters
-    # TODO: True params will change depending on usage of cdnow_sample or cdnow_full
-    r_true = 0.55
-    alpha_true = 10.58
-    s_true = 0.61
-    beta_true = 11.67
+    # TODO: Is this this needed?
+    def load_data(self, cdnow_sample):
+        return cdnow_sample()
 
-    rng = np.random.default_rng(34)
-
-    test_t = 30
-
-    # TODO: lifetimes.PNF predictive methods have different formulations
+    # TODO: lifetimes predictive methods have different formulations
     #       compared to their counterparts in ParetoNBDModel
     @classmethod
-    def setup_class(cls, cdnow_sample, cdnow_full):
-        # TODO: Is this wrapper even needed, or can the params just be set directly?
-        def pnf_params_wrapper(self, *args, **kwargs):
-            """
-            The methods from ParetoNBDFitter rely on a fitted model, i.e. estimates
-            for r, alpha, s, and beta parameters. This function circumvents model fitting
-            by assigning the fitted parameters from CDNOW_sample.csv directly.
-            """
-            return cls.r_true, cls.alpha_true, cls.s_true, cls.beta_true
+    def setup_class(cls):
+        # Set random seed
+        cls.rng = np.random.default_rng(34)
 
-        PNF._unload_params = pnf_params_wrapper
+        # Parameters
+        cls.r_true = 0.55
+        cls.alpha_true = 10.58
+        cls.s_true = 0.61
+        cls.beta_true = 11.67
 
-        PNF.conditional_expected_number_of_purchases_up_to_time = classmethod(
-            PNF.conditional_expected_number_of_purchases_up_to_time
+        # Specify priors here for convergence testing
+        cls.r_prior = None
+        cls.alpha_prior = None
+        cls.s_prior = None
+        cls.beta_prior = None
+
+        # test_data = cdnow_sample()
+        test_data = cls.load_data()
+        cls.customer_id = test_data.index
+        cls.frequency = test_data["frequency"]
+        cls.recency = test_data["recency"]
+        cls.T = test_data["T"]
+
+        # TODO: if cdnow_sample doesn't work here, create a fixture instead
+        # fit a model with find_MAP() for testing
+        # model = ParetoNBDModel(
+        # customer_id=cdnow_sample["customer_id"].values,
+        # frequency=cdnow_sample["frequency"].values,
+        # recency=cdnow_sample["recency"].values,
+        # T=cdnow_sample["T"].values,
+        # r_prior = r_prior,
+        # alpha_prior = alpha_prior,
+        # s_prior = s_prior,
+        # beta_prior = beta_prior,
+        # )
+        # model.fit(fitting_method="map", random_seed=self.rng)
+
+        cls.lifetimes_model = ParetoNBDFitter
+
+        cls.lifetimes_model._unload_params = (
+            cls.r_true,
+            cls.alpha_true,
+            cls.s_true,
+            cls.beta_true,
         )
 
-        cls.expected_test_num_purchases = (
-            PNF.conditional_expected_number_of_purchases_up_to_time(
-                t=cdnow_sample.test_t,
-                frequency=cdnow_sample.test_frequency,
-                recency=cdnow_sample.test_recency,
-                T=cdnow_sample.test_T,
-            )
-        )
-
-        PNF.expected_number_of_purchases_up_to_time = classmethod(
-            PNF.expected_number_of_purchases_up_to_time
-        )
-
-        cls.expected_test_num_purchases_new_customer = (
-            PNF.expected_number_of_purchases_up_to_time(t=cls.test_t)
-        )
-
-    @pytest.mark.parametrize("r_prior", (None, pm.Gamma.dist(1, 1)))
-    @pytest.mark.parametrize("alpha_prior", (None, pm.Gamma.dist(1, 1)))
-    @pytest.mark.parametrize("s_prior", (None, pm.Gamma.dist(1, 1)))
-    @pytest.mark.parametrize("beta_prior", (None, pm.Gamma.dist(1, 1)))
+    @pytest.mark.parametrize(
+        "r_prior, alpha_prior, s_prior, beta_prior",
+        [
+            (None, None, None, None),
+            (
+                pm.Gamma.dist(1, 1),
+                pm.Gamma.dist(1, 1),
+                pm.Gamma.dist(1, 1),
+                pm.Gamma.dist(1, 1),
+            ),
+        ],
+    )
     def test_model(self, r_prior, alpha_prior, s_prior, beta_prior):
         model = ParetoNBDModel(
             customer_id=self.customer_id,
@@ -99,34 +111,24 @@ class TestParetoNBDModel:
             "beta_log__": (),
         }
 
-    # TODO: Instead of fitting both datasets, create a class property instead to switch between datasets?
-    # TODO: Why can't the cdnow_sample and cdnow_full fixtures be called in this parametrize?
     @pytest.mark.slow
-    @pytest.mark.parametrize(
-        "dataset, rtol",
-        (
-            [None, 0.3],
-            [None, 0.1],
-        ),
-    )
-    def test_model_convergence(self, dataset, rtol):
-        # fit the model to CDNOW_sample with N=2,357 customers
-        # fit the model to CDNOW_full with N=23,570 customers
+    def test_model_convergence(self, cdnow_sample):
         # TODO: casting these Pandas Series to numpy arrays with the .values suffix
         #      was recommended due to bugs in lifetimes. try also testing with PD series because
         #      data preprocessing is now handled by pymc_marketing.clv.utils.clv_summary
+
         model = ParetoNBDModel(
-            customer_id=dataset["customer_id"].values,
-            frequency=dataset["frequency"].values,
-            recency=dataset["recency"].values,
-            T=dataset["T"].values,
+            customer_id=cdnow_sample["customer_id"].values,
+            frequency=cdnow_sample["frequency"].values,
+            recency=cdnow_sample["recency"].values,
+            T=cdnow_sample["T"].values,
         )
         model.fit(chains=1, progressbar=False, random_seed=self.rng)
         fit = model.fit_result.posterior
         np.testing.assert_allclose(
             [fit["r"].mean(), fit["alpha"].mean(), fit["s"].mean(), fit["beta"].mean()],
             [self.r_true, self.alpha_true, self.s_true, self.beta_true],
-            rtol=rtol,
+            rtol=1,
         )
 
     def test_model_repr(self):
@@ -139,110 +141,86 @@ class TestParetoNBDModel:
             "\nlikelihood~Potential(f(r,alpha,b,a))"
         )
 
-    def test_expected_num_purchases(self):
-        """
-        TODO: should we combine this test and the one below?
-        """
-        est_num_purchases = self.fixed_model.expected_num_purchases(
-            list(range(20, 40, 2)),
-            self.test_t,
-            self.test_frequency,
-            self.test_recency,
-            self.test_T,
+    @pytest.mark.skip(reason="Still a WIP")
+    @pytest.mark.parametrize("test_t", [1, 2, 3, 4, 5, 6])
+    def test_expected_purchases(self, test_t):
+        true_purchases = (
+            self.lifetimes_model.conditional_expected_number_of_purchases_up_to_time(
+                t=test_t,
+                frequency=self.frequency,
+                recency=self.recency,
+                T=self.T,
+            )
         )
 
-        assert est_num_purchases.shape == (1, 1000, 10)
+        est_num_purchases = self.model.expected_purchases(test_t)
+
+        assert est_num_purchases.shape == (1, 1000, len(self.customer_id))
         assert est_num_purchases.dims == ("chain", "draw", "customer_id")
 
         np.testing.assert_allclose(
-            self.expected_test_num_purchases,
+            true_purchases,
             est_num_purchases.mean(("chain", "draw")),
             rtol=0.1,
         )
 
-    def test_expected_num_purchases_new_customer(self):
-        est_num_purchases_new = self.fixed_model.expected_num_purchases_new_customer(
-            self.test_t
+    @pytest.mark.skip(reason="Still a WIP")
+    @pytest.mark.parametrize("test_t", [1, 2, 3, 4, 5, 6])
+    def test_expected_purchases_new_customer(self, test_t):
+        true_purchases_new = (
+            self.lifetimes_model.expected_number_of_purchases_up_to_time(
+                t=test_t,
+            )
         )
 
-        assert est_num_purchases_new.shape == (1, 1000, 10)
-        assert est_num_purchases_new.dims == ("chain", "draw", "t")
+        est_purchases_new = self.model.expected_num_purchases_new_customer(self.test_t)
+
+        assert est_purchases_new.shape == (1, 1000, 1)
+        assert est_purchases_new.dims == ("chain", "draw", "t")
 
         np.testing.assert_allclose(
-            self.expected_test_num_purchases,
-            est_num_purchases.mean(("chain", "draw")),
+            true_purchases_new,
+            est_purchases_new.mean(("chain", "draw")),
             rtol=1,
         )
 
-    def test_expected_probability_alive(self):
-        """
-        The "true" prefix refers to the value obtained using 1) the closed form
-        solution and 2) the data-generating parameter values.
-        """
-
-        est_prob_alive = self.model.expected_probability_alive(
-            self.customer_id,
-            self.frequency,
-            self.recency,
-            self.T,
+    @pytest.mark.skip(reason="Still a WIP")
+    @pytest.mark.parametrize("test_t", [1, 2, 3, 4, 5, 6])
+    def test_expected_probability_alive(self, test_t):
+        true_prob_alive = self.lifetimes_model.expected_number_of_purchases_up_to_time(
+            frequency=self.frequency,
+            recency=self.recency,
+            T=self.T,
         )
 
-        assert est_prob_alive.shape == (1, 1000, len(cls.dataset))
+        est_prob_alive = self.model.probability_alive(test_t)
+
+        assert est_prob_alive.shape == (1, 1000, len(self.customer_id))
         assert est_prob_alive.dims == ("chain", "draw", "customer_id")
 
         np.testing.assert_allclose(
-            self.true_prob_alive.mean(),
+            true_prob_alive.mean(),
             est_prob_alive.mean(),
             rtol=0.05,
         )
 
-    def test_expected_purchases_probability_new_customer(self):
+    @pytest.mark.skip(reason="Still a WIP")
+    @pytest.mark.parametrize("test_n, test_t", [(0, 0), (1, 1), (2, 2)])
+    def test_expected_purchases_probability(self, test_n, test_t):
+        true_prob_purchase = self.lifetimes_model.expected_purchases_probability(
+            frequency=self.frequency,
+            recency=self.recency,
+            T=self.T,
+        )
         est_purchases_new_customer = (
-            self.model.expected_purchases_probability_new_customer(self.n, self.test_t)
+            self.model.expected_purchases_probability_new_customer(n=test_n, t=test_t)
         )
 
-        assert est_purchases_new_customer.shape == (1, 1000, len(cls.dataset))
+        assert est_purchases_new_customer.shape == (1, 1000, len(self.customer_id))
         assert est_purchases_new_customer.dims == ("chain", "draw", "customer_id")
 
         np.testing.assert_allclose(
-            self.expected_purchases_new_customer.mean(),
+            true_prob_purchase.mean(),
             est_purchases_new_customer.mean(),
-            rtol=0.05,
-        )
-
-    def test_expected_purchases_probability(self):
-        est_purchase_probability = self.model.expected_purchases_probability(
-            self.n,
-            self.t,
-            self.customer_id,
-            self.frequency,
-            self.recency,
-            self.T,
-        )
-
-        assert est_purchase_probability.shape == (1, 1000, len(cls.dataset))
-        assert est_purchase_probability.dims == ("chain", "draw", "customer_id")
-
-        np.testing.assert_allclose(
-            self.expected_purchase_probability.mean(),
-            est_purchase_probability.mean(),
-            rtol=0.05,
-        )
-
-    def test_expected_future_probability_alive(self):
-        est_prob_alive_future = self.model.expected_future_probability_alive(
-            self.t,
-            self.customer_id,
-            self.frequency,
-            self.recency,
-            self.T,
-        )
-
-        assert est_prob_alive_future.shape == (1, 1000, len(cls.dataset))
-        assert est_prob_alive_future.dims == ("chain", "draw", "customer_id")
-
-        np.testing.assert_allclose(
-            self.true_prob_alive.mean(),
-            est_prob_alive_future.mean(),
             rtol=0.05,
         )
