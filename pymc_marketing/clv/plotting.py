73204ec63c72c79f18c0260891d620a625e0e54a
Will Dean <william.dean@free-now.com>
2023-08-05 16:39:56 +0200

    add func to plot exposure

--- pymc_marketing/clv/plotting.py
+++ pymc_marketing/clv/plotting.py
@@ -1,10 +1,95 @@
+from typing import Optional, Sequence
+
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from matplotlib.lines import Line2D
 
 __all__ = [
+    "plot_customer_exposure",
     "plot_frequency_recency_matrix",
     "plot_probability_alive_matrix",
 ]
+
+
+def plot_customer_exposure(
+    df: pd.DataFrame,
+    linewidth: Optional[float] = None,
+    size: Optional[float] = None,
+    labels: Optional[Sequence[str]] = None,
+    padding: float = 0.25,
+    ax: Optional[plt.Axes] = None,
+) -> plt.Axes:
+    """
+
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        A DataFrame with columns "recency" and "T" representing the recency and age of customers.
+    linewidth: float, optional
+        The width of the horizontal lines in the plot. Default is 0.1.
+    size: float, optional
+        The size of the markers in the plot. Default is 10.
+    labels: Sequence[str], optional
+        A sequence of labels for the legend. Default is ["Recency", "T"].
+    padding: float, optional
+        The padding around the plot. Default is 0.25.
+    ax: plt.Axes, optional
+        A matplotlib axes object to plot on. If None, a new figure and axes is created.
+
+    Returns
+    -------
+    ax: plt.Axes
+
+    Examples
+    --------
+    Plot customer exposure
+
+    .. code-block:: python
+
+        df = pd.DataFrame({
+            "recency": [1, 2, 3, 4, 5],
+            "T": [5, 5, 5, 5, 5]
+        })
+
+        plot_customer_exposure(df)
+
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    n = len(df)
+    customer_idx = np.arange(1, n + 1)
+
+    recency = df["recency"].to_numpy()
+    T = df["T"].to_numpy()
+
+    ax.hlines(y=customer_idx, xmin=0, xmax=recency, linewidth=linewidth, color="C0")
+    ax.hlines(y=customer_idx, xmin=recency, xmax=T, linewidth=linewidth, color="C1")
+
+    ax.scatter(x=recency, y=customer_idx, c="C0", linewidth=linewidth, s=size)
+    ax.scatter(x=T, y=customer_idx, c="C1", linewidth=linewidth, s=size)
+
+    ax.set(
+        xlabel="Time since first purchase",
+        ylabel="Customer",
+        xlim=(0 - padding, T.max() + padding),
+        ylim=(1 - padding, n + padding),
+        title="Customer Exposure",
+    )
+
+    if labels is None:
+        labels = ["Recency", "T"]
+
+    legend_elements = [
+        Line2D([0], [0], color="C0", label=labels[0]),
+        Line2D([0], [0], color="C1", label=labels[1]),
+    ]
+
+    ax.legend(handles=legend_elements, loc="best")
+
+    return ax
 
 
 def plot_frequency_recency_matrix(
