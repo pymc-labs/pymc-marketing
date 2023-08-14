28b233a8f77db6fdf2e83bfdebf3ec0efbaa9e22
Will Dean <william.dean@free-now.com>
2023-08-14 20:19:26 +0200

    test other two plotting functions

--- tests/clv/test_plotting.py
+++ tests/clv/test_plotting.py
@@ -1,8 +1,17 @@
+from typing import Union
+
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import pytest
+import xarray as xr
+from pytensor.tensor import TensorVariable
 
-from pymc_marketing.clv.plotting import plot_customer_exposure
+from pymc_marketing.clv.plotting import (
+    plot_customer_exposure,
+    plot_frequency_recency_matrix,
+    plot_probability_alive_matrix,
+)
 
 
 @pytest.fixture
@@ -48,3 +57,95 @@ def test_plot_cumstomer_exposure_with_ax(test_summary_data) -> None:
 def test_plot_customer_exposure_invalid_args(test_summary_data, kwargs) -> None:
     with pytest.raises(ValueError):
         plot_customer_exposure(test_summary_data, **kwargs)
+
+
+class MockModel:
+    def __init__(self, frequency, recency):
+        self.frequency = frequency
+        self.recency = recency
+
+    def _mock_posterior(
+        self, customer_id: Union[np.ndarray, pd.Series]
+    ) -> xr.DataArray:
+        n_customers = len(customer_id)
+        n_chains = 4
+        n_draws = 10
+        chains = np.arange(n_chains)
+        draws = np.arange(n_draws)
+        return xr.DataArray(
+            data=np.ones((n_customers, n_chains, n_draws)),
+            coords={"customer_id": customer_id, "chain": chains, "draw": draws},
+            dims=["customer_id", "chain", "draw"],
+        )
+
+    def expected_probability_alive(
+        self,
+        customer_id: Union[np.ndarray, pd.Series],
+        frequency: Union[np.ndarray, pd.Series],
+        recency: Union[np.ndarray, pd.Series],
+        T: Union[np.ndarray, pd.Series],
+    ):
+        return self._mock_posterior(customer_id)
+
+    def expected_num_purchases(
+        self,
+        customer_id: Union[np.ndarray, pd.Series],
+        t: Union[np.ndarray, pd.Series, TensorVariable],
+        frequency: Union[np.ndarray, pd.Series, TensorVariable],
+        recency: Union[np.ndarray, pd.Series, TensorVariable],
+        T: Union[np.ndarray, pd.Series, TensorVariable],
+    ):
+        return self._mock_posterior(customer_id)
+
+
+@pytest.fixture
+def mock_model(test_summary_data) -> MockModel:
+    return MockModel(test_summary_data["frequency"], test_summary_data["recency"])
+
+
+def test_plot_frequency_recency_matrix(mock_model) -> None:
+    ax: plt.Axes = plot_frequency_recency_matrix(mock_model)
+
+    assert isinstance(ax, plt.Axes)
+
+
+def test_plot_frequency_recency_matrix_bounds(mock_model) -> None:
+    max_recency = 10
+    max_frequency = 10
+    ax: plt.Axes = plot_frequency_recency_matrix(
+        mock_model, max_recency=max_recency, max_frequency=max_frequency
+    )
+
+    assert isinstance(ax, plt.Axes)
+
+
+def test_plot_frequency_recency_matrix_with_ax(mock_model) -> None:
+    ax = plt.subplot()
+    plot_frequency_recency_matrix(mock_model, ax=ax)
+
+    assert ax.get_xlabel() == "Customer's Historical Frequency"
+    assert ax.get_ylabel() == "Customer's Recency"
+
+
+def test_plot_probability_alive_matrix(mock_model) -> None:
+    ax: plt.Axes = plot_probability_alive_matrix(mock_model)
+
+    assert isinstance(ax, plt.Axes)
+
+
+def test_plot_probability_alive_matrix_bounds(mock_model) -> None:
+    max_recency = 10
+    max_frequency = 10
+    ax: plt.Axes = plot_probability_alive_matrix(
+        mock_model, max_recency=max_recency, max_frequency=max_frequency
+    )
+
+    assert isinstance(ax, plt.Axes)
+
+
+def test_plot_probability_alive_matrix_with_ax(mock_model) -> None:
+    ax = plt.subplot()
+    plot_probability_alive_matrix(mock_model, ax=ax)
+
+    assert ax.get_xlabel() == "Customer's Historical Frequency"
+    assert ax.get_ylabel() == "Customer's Recency"
