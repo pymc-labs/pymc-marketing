f5d5014a07581547bfb23df8d6e6a1c22046194a
nialloulton <niall.oulton@1749.io>
2023-10-25 10:06:02 +0300

    TVP Fix Channel

--- pymc_marketing/mmm/delayed_saturated_mmm.py
+++ pymc_marketing/mmm/delayed_saturated_mmm.py
@@ -213,6 +213,11 @@ class BaseDelayedSaturatedMMM(MMM):
                 var=logistic_saturation(x=channel_adstock, lam=lam),
                 dims=("date", "channel"),
             )
+
+            #Debug for shape error of channel contributions
+            print("Shape of channel_adstock_saturated:", channel_adstock_saturated.shape.eval())
+            print("Shape of beta_channel:", beta_channel.shape.eval())
+
             channel_contributions = pm.Deterministic(
                 name="channel_contributions",
                 var=channel_adstock_saturated * beta_channel,
