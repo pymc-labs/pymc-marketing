5424e6bc3aea233917976d62db7129b77d837ebe
Eirik Aalstad Baekkelund <89584092+eirikbaekkelund@users.noreply.github.com>
2024-01-07 15:49:11 +0100

    remove docstring arg to _get_weights

--- pymc_marketing/mmm/delayed_saturated_mmm.py
+++ pymc_marketing/mmm/delayed_saturated_mmm.py
@@ -892,7 +892,7 @@ class DelayedSaturatedMMM(
             The maximum lag for the adstock transformation, by default 12
         normalize : bool, optional
             Whether to normalize the weights, by default True
-     
+
         Returns
         -------
         Union[float, np.ndarray]
