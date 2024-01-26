2f2d81445ec60b9151563ef0b3699491b62f3174
Abdalaziz Rashid <abdalaziz.rashid@outlook.com>
2024-01-26 19:57:52 +0300

    Swap Before and After convolution modes as per #489 (#501)
    * Add support for string mode args
    
    * Swap before and after and make mode explicit
    
    * Use Union due Python 3.9
    
    * Style

--- tests/mmm/test_transformers.py
+++ tests/mmm/test_transformers.py
@@ -69,10 +69,10 @@ def test_batched_convolution(convolution_inputs, convolution_axis, mode):
     x_val = np.moveaxis(
         x_val if x_val is not None else getattr(x, "value", x), convolution_axis, 0
     )
-    if mode == ConvMode.Before:
+    if mode == ConvMode.After:
         np.testing.assert_allclose(y_val[0], x_val[0])
         np.testing.assert_allclose(y_val[1:], x_val[1:] + x_val[:-1])
-    elif mode == ConvMode.After:
+    elif mode == ConvMode.Before:
         np.testing.assert_allclose(y_val[-1], x_val[-1])
         np.testing.assert_allclose(y_val[:-1], x_val[1:] + x_val[:-1])
     elif mode == ConvMode.Overlap:
