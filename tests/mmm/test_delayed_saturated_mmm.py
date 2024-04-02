dec4a80a73f3303b4bb7f5e8854bdd8786aa5ee3
Will Dean <57733339+wd60622@users.noreply.github.com>
2024-04-02 15:07:06 +0200

    MMM Lift test calibration (#590)
    * add scaling into lift test method
    
    * test scaling methods
    
    * test change of MMM likelihood
    
    * link up beta_channel parameters
    
    * change name to sigma
    
    * reorganize and edit
    
    * closes #406
    
    * address the feedback in docstrings
    
    * add more to docstring
    
    * format the docstring
    
    * be verbose for future devs
    
    * be explicit for the column max values
    
    * incorporate the feedback
    
    * hide cells and add to intro
    
    * add conclusion
    
    * switch to header 2
    
    * run notebook through
    
    * move the types together
    
    * separate model estimated from empirical
    
    * fix typos

--- tests/mmm/test_delayed_saturated_mmm.py
+++ tests/mmm/test_delayed_saturated_mmm.py
@@ -858,3 +858,43 @@ def test_plot_new_spend_contributions_prior_select_channels(
     )
 
     assert isinstance(ax, plt.Axes)
+
+
+@pytest.fixture
+def df_lift_test() -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            "channel": ["channel_1", "channel_1"],
+            "x": [1, 2],
+            "delta_x": [1, 1],
+            "delta_y": [1, 1],
+            "sigma": [1, 1],
+        }
+    )
+
+
+def test_add_lift_test_measurements(mmm, toy_X, toy_y, df_lift_test) -> None:
+    mmm.build_model(X=toy_X, y=toy_y)
+
+    name = "lift_measurements"
+    assert name not in mmm.model
+
+    mmm.add_lift_test_measurements(
+        df_lift_test,
+        name=name,
+    )
+
+    assert name in mmm.model
+
+
+def test_add_lift_test_measurements_no_model() -> None:
+    mmm = DelayedSaturatedMMM(
+        date_column="date",
+        channel_columns=["channel_1", "channel_2"],
+        adstock_max_lag=4,
+        control_columns=["control_1", "control_2"],
+    )
+    with pytest.raises(RuntimeError, match="The model has not been built yet."):
+        mmm.add_lift_test_measurements(
+            pd.DataFrame(),
+        )
