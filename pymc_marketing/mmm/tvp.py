19d13d87b824c807b589e3efb76d6a58299ffe89
ulfaslak <ulfjensen@gmail.com>
2024-04-19 21:34:43 +0200

    resolve review comments

--- pymc_marketing/mmm/tvp.py
+++ pymc_marketing/mmm/tvp.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pymc as pm
 import pytensor.tensor as pt
 from pytensor.tensor import softplus
@@ -6,10 +7,10 @@ from pytensor.tensor import softplus
 def time_varying_prior(
     name: str,
     X: pt.sharedvar.TensorSharedVariable,
-    X_mid: int | float,
     dims: tuple[str, str] | str,
-    m: int,
-    L: int | float,
+    X_mid: int | float | None = None,
+    m: int = 200,
+    L: int | float | None = None,
     eta_lam: float = 1,
     ls_mu: float = 5,
     ls_sigma: float = 5,
@@ -22,25 +23,29 @@ def time_varying_prior(
     Parameters
     ----------
     name : str
-        Name of the prior.
+        Name of the prior and associated variables.
     X : 1d array-like of int or float
         Time points.
     X_mid : int or float
         Midpoint of the time points.
     dims : tuple of str or str
-        Dimensions of the prior.
+        Dimensions of the prior. If a tuple, the first element is the name of
+        the time dimension, and the second may be any other dimension, across
+        which independent time varying priors for each coordinate are desired
+        (e.g. channels).
     m : int
         Number of basis functions.
     L : int
         Extent of basis functions. Set this to reflect the expected range of
-        in+out-of-sample data (considering that time-indices are zero-centered)
+        in+out-of-sample data (considering that time-indices are zero-centered).
+        Default is `X_mid * 2` (identical to `c=2` in HSGP).
     eta_lam : float
         Exponential prior for the variance.
     ls_mu : float
         Mean of the inverse gamma prior for the lengthscale.
     ls_sigma : float
         Standard deviation of the inverse gamma prior for the lengthscale.
-    cov_func : pm.gp.cov.Prod
+    cov_func : pm.gp.cov.Covariance
         Covariance function.
 
     Returns
@@ -57,19 +62,24 @@ def time_varying_prior(
         Regression.
     """
 
+    if X_mid is None:
+        X_mid = float(X.mean().eval())
+    if L is None:
+        L = X_mid * 2
+
     with pm.modelcontext(None) as model:
         if cov_func is None:
-            eta = pm.Exponential(f"eta_{name}", lam=eta_lam)
-            ls = pm.InverseGamma(f"ls_{name}", mu=ls_mu, sigma=ls_sigma)
+            eta = pm.Exponential(f"{name}_eta", lam=eta_lam)
+            ls = pm.InverseGamma(f"{name}_ls", mu=ls_mu, sigma=ls_sigma)
             cov_func = eta**2 * pm.gp.cov.Matern52(1, ls=ls)
 
-        hsgp_size: int | tuple[int, int] = m
+        model.add_coord("m", np.arange(m))  # type: ignore
+        hsgp_dims: str | tuple[str, str] = "m"
         if isinstance(dims, tuple):
-            n_columns = len(model.coords[dims[1]])
-            hsgp_size = (n_columns, m)
+            hsgp_dims = (dims[0], "m")
 
         gp = pm.gp.HSGP(m=[m], L=[L], cov_func=cov_func)
         phi, sqrt_psd = gp.prior_linearized(Xs=X[:, None] - X_mid)
-        hsgp_coefs = pm.Normal(f"_hsgp_coefs_{name}", size=hsgp_size)
+        hsgp_coefs = pm.Normal(f"{name}_hsgp_coefs", dims=hsgp_dims)
         f = softplus(phi @ (hsgp_coefs * sqrt_psd).T)
         return pm.Deterministic(name, f, dims=dims)
