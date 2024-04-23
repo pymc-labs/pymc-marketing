0374d3bdee8a9c8cf1493c08849aad264335e349
Will Dean <wd60622@gmail.com>
2024-04-23 16:43:49 +0200

    rewrite of the components

---
+++ pymc_marketing/mmm/models/components/base.py
@@ -0,0 +1,120 @@
+from inspect import signature
+from typing import Any, Optional
+
+from pytensor import tensor as pt
+
+from pymc_marketing.mmm.utils import _get_distribution
+
+
+class ParameterPriorException(Exception):
+    """Error when the functions and specified priors don't match up."""
+
+    def __init__(self, priors: set[str], parameters: set[str]) -> None:
+        self.priors = priors
+        self.parameters = parameters
+
+        msg = "The function parameters and priors don't line up."
+
+        if self.priors:
+            msg = f"{msg} Missing prior: {self.priors}."
+
+        if self.parameters:
+            msg = f"{msg} Missing parameter: {self.parameters}."
+
+        super().__init__(msg)
+
+
+class Transformation:
+    """Base class for adstock and saturation functions.
+
+    The subclasses will need to implement the following attributes:
+
+    - function: The function that will be applied to the data.
+    - prefix: The prefix for the variables that will be created.
+    - default_priors: The default priors for the parameters of the function.
+
+    In order to make a new saturation or adstock function, this subclass would
+    not be used. Instead, the subclasses would be used.
+
+    """
+
+    prefix: str
+    default_priors: dict[str, Any]
+    function: Any
+
+    def __init__(self, priors: Optional[dict] = None) -> None:
+        self._checks()
+
+        priors = priors or {}
+        self.function_priors = {**self.default_priors, **priors}
+
+    def _checks(self) -> None:
+        self._has_all_attributes()
+        self._function_works_on_instances()
+        self._has_defaults_for_all_arguments()
+
+    def _has_all_attributes(self) -> None:
+        if not hasattr(self, "prefix"):
+            raise NotImplementedError("prefix must be implemented in the subclass")
+
+        if not hasattr(self, "default_priors"):
+            raise NotImplementedError(
+                "default_priors must be implemented in the subclass"
+            )
+
+        if not hasattr(self, "function"):
+            raise NotImplementedError("function must be implemented in the subclass")
+
+    def _has_defaults_for_all_arguments(self) -> None:
+        function_signature = signature(self.function)
+
+        # Remove the first one
+        parameters_that_need_priors = set(
+            list(function_signature.parameters.keys())[1:]
+        )
+        parameters_with_priors = set(self.default_priors.keys())
+
+        missing_priors = parameters_that_need_priors - parameters_with_priors
+        missing_parameters = parameters_with_priors - parameters_that_need_priors
+
+        if missing_priors or missing_parameters:
+            raise ParameterPriorException(missing_priors, missing_parameters)
+
+    def _function_works_on_instances(self) -> None:
+        n_parameters = len(signature(self.function).parameters)
+
+        n_default_parameters = len(self.default_priors)
+
+        if n_parameters - 1 == n_default_parameters:
+            return
+
+        setattr(self, "function", self.__class__.function)
+
+    @property
+    def variable_mapping(self) -> dict[str, str]:
+        return {
+            parameter: f"{self.prefix}_{parameter}"
+            for parameter in self.default_priors.keys()
+        }
+
+    def _create_distributions(self, dim_name: str) -> dict[str, pt.TensorVariable]:
+        distributions: dict[str, pt.TensorVariable] = {}
+        for parameter_name, variable_name in self.variable_mapping.items():
+            parameter_prior = self.function_priors[parameter_name]
+
+            distribution = _get_distribution(
+                dist=parameter_prior,
+            )
+
+            distributions[parameter_name] = distribution(
+                name=variable_name,
+                dims=dim_name,
+                **parameter_prior["kwargs"],
+            )
+
+        return distributions
+
+    def apply(self, x):
+        """Called within a model context."""
+        kwargs = self._create_distributions(dim_name="channel")
+        return self.function(x, **kwargs)
--- pymc_marketing/mmm/models/components/saturation.py
+++ pymc_marketing/mmm/models/components/saturation.py
@@ -1,285 +1,84 @@
-from inspect import signature
-from typing import Dict, Optional
+from typing import Dict, Type
 
-import pymc as pm
-from pytensor import tensor as pt
-
+from pymc_marketing.mmm.models.components.base import Transformation
 from pymc_marketing.mmm.transformers import (
     hill_saturation,
     logistic_saturation,
     michaelis_menten,
+    tanh_saturation,
+    tanh_saturation_baselined,
 )
-from pymc_marketing.mmm.utils import _get_distribution
 
 
-def function_parameters(func) -> set[str]:
-    return set(signature(func).parameters.keys())
+class SaturationTransformation(Transformation):
+    prefix: str = "saturation"
 
 
-class MissingSaturationParameters(Exception):
-    pass
+class LogisticSaturation(SaturationTransformation):
+    def function(self, x, lam, beta):
+        return beta * logistic_saturation(x, lam)
+
+    default_priors = {
+        "lam": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "beta": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+    }
 
 
-### SATURATION FUNCTIONS
-class BaseSaturationFunction:
-    """Core logic for checks for all saturation functions.
+class TanhSaturation(SaturationTransformation):
+    def function(self, x, b, c, beta):
+        return beta * tanh_saturation(x, b, c)
 
-    Subclasses need to define three items:
-
-    - saturation_function: Method representation of the saturation function
-    - variable_mapping: The mapping from saturation function parameters to model variable names
-    - default_saturation_config: The mapping from model variable names to prior distributions
-
-    """
-
-    def __init__(
-        self, model: Optional[pm.Model] = None, model_config: Optional[Dict] = None
-    ):
-        self.model = model
-        self.model_config = self._initialize_model_config(model_config)
-
-    def _initialize_model_config(self, model_config: Optional[Dict]) -> Dict:
-        REQUIRED_KEYS = list(self.default_saturation_config.keys())
-
-        if model_config is not None and not all(
-            key in model_config for key in REQUIRED_KEYS
-        ):
-            update_model_config = {**model_config, **self.default_saturation_config}
-        elif model_config is None:
-            update_model_config = self.default_saturation_config
-        else:
-            update_model_config = model_config
-        return update_model_config
-
-    @property
-    def default_saturation_config(self) -> Dict:
-        msg = (
-            "Subclasses should implement this method."
-            " This contains the model variable name and their distributions."
-        )
-        raise NotImplementedError(msg)
-
-    @property
-    def variable_mapping(self) -> dict[str, str]:
-        """Mapping between the saturation function args and model variables."""
-        msg = (
-            "Subclasses should implement this mapping from parameters"
-            " to model variable names"
-        )
-        raise NotImplementedError(msg)
-
-    def saturation_function(self, x, **kwargs):
-        """Saturation function to be used..."""
-
-    def _checks(self) -> None:
-        self._check_variable_mapping_has_all_saturation_function_parameters()
-
-    def _check_variable_mapping_has_all_saturation_function_parameters(self) -> None:
-        saturation_parameters = function_parameters(self.saturation_function)
-        variable_mapping_keys = set(self.variable_mapping.keys())
-
-        missing_parameters = (
-            saturation_parameters - set(["self", "x"]) - variable_mapping_keys
-        )
-        if missing_parameters:
-            msg = f"The saturation function has missing parameters {list(missing_parameters)}"
-            raise MissingSaturationParameters(msg)
-
-    def _create_distributions(self) -> dict[str, pt.TensorVariable]:
-        distributions: dict[str, pt.TensorVariable] = {}
-        for parameter_name, variable_name in self.variable_mapping.items():
-            distribution = _get_distribution(
-                dist=self.model_config[variable_name],
-            )
-            distributions[parameter_name] = distribution(
-                name=variable_name,
-                dims=("channel",),
-                **self.model_config[variable_name]["kwargs"],
-            )
-
-        return distributions
-
-    def apply(self, data):
-        kwargs = self._create_distributions()
-        return self.saturation_function(data, **kwargs)
+    default_priors = {
+        "b": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "c": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "beta": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+    }
 
 
-class HillSaturationComponent(BaseSaturationFunction):
-    """
-    A class representing the Hill Saturation component of a marketing mix model.
+class TanhSaturationBaselined(SaturationTransformation):
+    def function(self, x, x0, gain, r, beta):
+        return beta * tanh_saturation_baselined(x, x0, gain, r)
 
-    Parameters:
-    -----------
-    model : PyMC.Model
-        The PyMC model object.
-    model_config : dict
-        A dictionary containing the configuration parameters for the model.
-
-    Methods:
-    --------
-    apply(data: Union[pm.Data, pm.MutableData]) -> pm.Deterministic:
-        Apply the Hill Saturation component to the given data.
-
-    """
-
-    @property
-    def default_saturation_config(self) -> Dict:
-        return {
-            "saturation_sigma": {
-                "dist": "HalfNormal",
-                "kwargs": {"sigma": 2},
-            },
-            "saturation_lambda": {
-                "dist": "Gamma",
-                "kwargs": {"mu": 1, "sigma": 2},
-            },
-            "saturation_beta": {
-                "dist": "HalfNormal",
-                "kwargs": {"sigma": 2},
-            },
-        }
-
-    @property
-    def variable_mapping(self) -> Dict[str, str]:
-        """The mapping from saturation function parameters to variables in a model."""
-        return {
-            "lam": "saturation_lambda",
-            "beta": "saturation_beta",
-            "sigma": "saturation_sigma",
-        }
-
-    def saturation_function(self, x, sigma, beta, lam):
-        return hill_saturation(
-            x=x,
-            sigma=sigma,
-            beta=beta,
-            lam=lam,
-        )
+    default_priors = {
+        "x0": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "gain": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "r": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "beta": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+    }
 
 
-class MentenSaturationComponent(BaseSaturationFunction):
-    """
-    A class representing the Menten Saturation component of a marketing mix model.
+class MichaelisMentenSaturation(SaturationTransformation):
+    function = michaelis_menten
 
-    Parameters:
-    -----------
-    model : PyMC.Model
-        The PyMC model object.
-    model_config : dict
-        A dictionary containing the configuration parameters for the model.
-
-    Methods:
-    --------
-    apply(data: Union[pm.Data, pm.MutableData]) -> pm.Deterministic:
-        Apply the Menten Saturation component to the given data.
-
-    """
-
-    @property
-    def default_saturation_config(self) -> Dict:
-        return {
-            "saturation_alpha": {
-                "dist": "HalfNormal",
-                "kwargs": {"sigma": 2},
-            },
-            "saturation_lambda": {
-                "dist": "Gamma",
-                "kwargs": {"mu": 1, "sigma": 2},
-            },
-        }
-
-    @property
-    def variable_mapping(self) -> Dict[str, str]:
-        """The mapping from saturation function parameters to variables in a model."""
-        return {
-            "lam": "saturation_lambda",
-            "alpha": "saturation_alpha",
-        }
-
-    def saturation_function(self, x, alpha, lam):
-        return michaelis_menten(
-            x=x,
-            alpha=alpha,
-            lam=lam,
-        )
+    default_priors = {
+        "alpha": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "lam": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+    }
 
 
-class LogisticSaturationComponent(BaseSaturationFunction):
-    """
-    A class representing the Logistic Saturation component of a marketing mix model.
+class HillSaturation(SaturationTransformation):
+    function = hill_saturation
 
-    Parameters:
-    -----------
-    model : PyMC.Model
-        The PyMC model object.
-    model_config : dict
-        A dictionary containing the configuration parameters for the model.
+    default_priors = {
+        "sigma": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "beta": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+        "lam": {"dist": "HalfNormal", "kwargs": {"sigma": 1}},
+    }
 
-    Methods:
-    --------
-    apply(data: Union[pm.Data, pm.MutableData]) -> pm.Deterministic:
-        Apply the Logistic Saturation component to the given data.
 
-    """
-
-    @property
-    def default_saturation_config(self) -> Dict:
-        return {
-            "saturation_beta": {
-                "dist": "HalfNormal",
-                "kwargs": {"sigma": 2},
-            },
-            "saturation_lambda": {
-                "dist": "Gamma",
-                "kwargs": {"alpha": 3, "beta": 1},
-            },
-        }
-
-    @property
-    def variable_mapping(self) -> Dict[str, str]:
-        """The mapping from saturation function parameters to variables in a model."""
-        return {
-            "lam": "saturation_lambda",
-            "beta": "saturation_beta",
-        }
-
-    def saturation_function(self, x, beta, lam):
-        return beta * logistic_saturation(x=x, lam=lam)
+SATURATION_TRANSFORMATIONS: Dict[str, Type[SaturationTransformation]] = {
+    "logistic_saturation": LogisticSaturation,
+    "tanh": TanhSaturation,
+    "hill": HillSaturation,
+    "tanh_baselined": TanhSaturationBaselined,
+    "michaelis_menten": MichaelisMentenSaturation,
+}
 
 
 def _get_saturation_function(
-    name: str, model: Optional[pm.Model] = None, model_config: Optional[Dict] = None
-):
-    """
-    Get the saturation function based on the given name.
+    function: str | SaturationTransformation,
+) -> SaturationTransformation:
+    if isinstance(function, str):
+        return SATURATION_TRANSFORMATIONS[function]()  # type: ignore
 
-    Parameters:
-    -----------
-    name : str
-        The name of the saturation function.
-    model : pm.Model
-        The PyMC model object.
-    model_config : Optional[Dict], optional
-        A dictionary containing the configuration parameters for the model, by default None.
-
-    Returns:
-    --------
-    Union[HillSaturationComponent, MentenSaturationComponent, LogisticSaturationComponent]:
-        The saturation function object based on the given name.
-
-    Raises:
-    -------
-    ValueError:
-        If the saturation function name is not recognized.
-    """
-    saturation_functions = {
-        "hill": HillSaturationComponent,
-        "michaelis_menten": MentenSaturationComponent,
-        "logistic": LogisticSaturationComponent,
-        # Add other lagging function classes here as needed
-    }
-
-    if name in saturation_functions:
-        return saturation_functions[name](model=model, model_config=model_config)
-    else:
-        raise ValueError(f"Saturation function {name} not recognized.")
+    return function
