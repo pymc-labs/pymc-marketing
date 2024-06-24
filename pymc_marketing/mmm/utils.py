2f33a68684d1afe9640ee37b01f04ba1d9cc0c76
Will Dean <wd60622@gmail.com>
2024-06-24 07:56:05 +0200

    remove the previous dictionary function

--- pymc_marketing/mmm/utils.py
+++ pymc_marketing/mmm/utils.py
@@ -19,7 +19,6 @@ from typing import Any
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
-import pymc as pm
 import xarray as xr
 from scipy.optimize import curve_fit, minimize_scalar
 
@@ -394,32 +393,3 @@ def create_new_spend_data(
             spend,
         ]
     )
-
-
-def _get_distribution_from_dict(dist: dict) -> Callable:
-    """
-    Retrieve a PyMC distribution callable based on the provided dictionary.
-
-    Parameters
-    ----------
-    dist : Dict
-        A dictionary containing the key 'dist' which should correspond to the
-        name of a PyMC distribution.
-
-    Returns
-    -------
-    Callable
-        A PyMC distribution callable that can be used to instantiate a random
-        variable.
-
-    Raises
-    ------
-    ValueError
-        If the specified distribution name in the dictionary does not correspond
-        to any distribution in PyMC.
-    """
-    try:
-        prior_distribution = getattr(pm, dist["dist"])
-    except AttributeError:
-        raise ValueError(f"Distribution {dist['dist']} does not exist in PyMC")
-    return prior_distribution
