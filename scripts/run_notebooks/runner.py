7c2b778ce049364ec179e5f31a1cabf84adf1a29
juanitorduz <juan.orduz@wolt.com>
2024-06-28 12:18:08 +0200

    paralelize and Path

--- scripts/run_notebooks/runner.py
+++ scripts/run_notebooks/runner.py
@@ -1,11 +1,19 @@
 import logging
-import os
+from pathlib import Path
 
 import papermill
+from joblib import Parallel, delayed
 from tqdm import tqdm
 
 KERNEL_NAME: str = "python3"
-NOTEBOOKS_PATH: str = "docs/source/notebooks"
+NOTEBOOKS_PATH = Path("docs/source/notebooks")
+NOTEBOOKS_SKIP: list[str] = [
+    "clv_quickstart.ipynb",
+    "mmm_budget_allocation_example.ipynb",
+    "mmm_tvp_example.ipynb",
+]
+NOTEBOOKS: list[Path] = list(NOTEBOOKS_PATH.glob("*/*.ipynb"))
+NOTEBOOKS = [nb for nb in NOTEBOOKS if nb.name not in NOTEBOOKS_SKIP]
 
 
 def setup_logging() -> None:
@@ -15,41 +23,15 @@ def setup_logging() -> None:
     )
 
 
-def get_notebooks_names() -> list[str]:
-    return [
-        "clv/bg_nbd.ipynb",
-        # "clv/clv_quickstart.ipynb",
-        "clv/gamma_gamma.ipynb",
-        "clv/pareto_nbd.ipynb",
-        "clv/sBG.ipynb",
-        "mmm/mmm_budget_allocation_example.ipynb",
-        "mmm/mmm_components.ipynb",
-        "mmm/mmm_example.ipynb",
-        "mmm/mmm_lift_test.ipynb",
-        "mmm/mmm_tvp_example.ipynb",
-    ]
+def get_cwd_from_notebook_path(notebook_path: Path) -> str:
+    return str(notebook_path).rsplit("/", 1)[0]
 
 
-def get_notebooks_paths() -> list[str]:
-    return [
-        f"{NOTEBOOKS_PATH}/{notebook_name}" for notebook_name in get_notebooks_names()
-    ]
-
-
-def verify_notebooks_exist() -> None:
-    for notebook_path in get_notebooks_paths():
-        if not os.path.exists(notebook_path):
-            raise FileNotFoundError(f"Notebook not found: {notebook_path}")
-
-
-def get_cwd_from_notebook_path(notebook_path: str) -> str:
-    return notebook_path.rsplit("/", 1)[0]
-
-
-def run_notebook(notebook_path: str) -> None:
+def run_notebook(notebook_path: Path) -> None:
     cwd = get_cwd_from_notebook_path(notebook_path)
+    logging.info(f"Running notebook: {notebook_path.name}")
     papermill.execute_notebook(
-        input_path=notebook_path,
+        input_path=str(notebook_path),
         output_path=None,
         kernel_name=KERNEL_NAME,
         cwd=cwd,
@@ -59,12 +41,8 @@ def run_notebook(notebook_path: str) -> None:
 if __name__ == "__main__":
     setup_logging()
     logging.info("Starting notebook runner")
-
-    notebooks_paths = get_notebooks_paths()
-    verify_notebooks_exist()
-
-    for notebook_path in tqdm(notebooks_paths):
-        logging.info(f"Running notebook: {notebook_path}")
-        run_notebook(notebook_path)
+    Parallel(n_jobs=-1)(
+        delayed(run_notebook)(notebook_path) for notebook_path in tqdm(NOTEBOOKS)
+    )
 
     logging.info("Notebooks run successfully!")
