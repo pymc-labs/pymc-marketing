9f793221dda627f5cf6c5b1e986d97075c57d48f
Will Dean <wd60622@gmail.com>
2024-10-31 17:39:42 -0400

    add docstrings to synthetic data module

--- pymc_marketing/customer_choice/synthetic_data.py
+++ pymc_marketing/customer_choice/synthetic_data.py
@@ -25,9 +25,45 @@ def generate_saturated_data(
     market_shares_before,
     market_shares_after,
     market_share_labels,
-    rng: np.random.Generator,
+    random_seed: int | np.random.Generator | None = None,
 ):
-    """Generate synthetic data for the MVITS model, assuming market is saturated."""
+    """Generate synthetic data for the MVITS model, assuming market is saturated.
+
+    This function generates synthetic data for the MVITS model, assuming that the market is
+    saturated. This makes the assumption that the total sales are normally distributed around
+    some average level of sales, and that the market shares are constant over time.
+
+    Parameters
+    ----------
+    total_sales_mu: int
+        The average level of sales in the market.
+    total_sales_sigma: float
+        The standard deviation of sales in the market.
+    treatment_time: int
+        The time at which the new model is introduced.
+    n_observations: int
+        The number of observations to generate.
+    market_shares_before: list[float]
+        The market shares before the introduction of the new model.
+    market_shares_after: list[float]
+        The market shares after the introduction of the new model.
+    market_share_labels: list[str]
+        The labels for the market shares.
+    random_seed: np.random.Generator | int, optional
+        The random number generator to use.
+
+    Returns
+    -------
+    data: pd.DataFrame
+        The synthetic data generated.
+
+    """
+    rng: np.random.Generator = (
+        random_seed
+        if isinstance(random_seed, np.random.Generator)
+        else np.random.default_rng(random_seed)
+    )
+
     rates = np.array(
         treatment_time * market_shares_before
         + (n_observations - treatment_time) * market_shares_after
@@ -62,13 +98,50 @@ def generate_unsaturated_data(
     market_shares_before: list[list[float]],
     market_shares_after: list[list[float]],
     market_share_labels: list[str],
-    rng: np.random.Generator,
+    random_seed: np.random.Generator | int | None = None,
 ):
     """Generate synthetic data for the MVITS model.
 
     Notably, we can define different total sales levels before and after the
     introduction of the new model.
+
+    This function generates synthetic data for the MVITS model, assuming that the market is
+    unsaturated meaning that there are new sales to be made.
+
+    This makes the assumption that the total sales are normally distributed around
+    some average level of sales, and that the market shares are constant over time.
+
+    Parameters
+    ----------
+    total_sales_mu: int
+        The average level of sales in the market.
+    total_sales_sigma: float
+        The standard deviation of sales in the market.
+    treatment_time: int
+        The time at which the new model is introduced.
+    n_observations: int
+        The number of observations to generate.
+    market_shares_before: list[float]
+        The market shares before the introduction of the new model.
+    market_shares_after: list[float]
+        The market shares after the introduction of the new model.
+    market_share_labels: list[str]
+        The labels for the market shares.
+    random_seed: np.random.Generator | int, optional
+        The random number generator to use.
+
+    Returns
+    -------
+    data: pd.DataFrame
+        The synthetic data generated.
+
     """
+    rng: np.random.Generator = (
+        random_seed
+        if isinstance(random_seed, np.random.Generator)
+        else np.random.default_rng(random_seed)
+    )
+
     rates = np.array(
         treatment_time * market_shares_before
         + (n_observations - treatment_time) * market_shares_after
