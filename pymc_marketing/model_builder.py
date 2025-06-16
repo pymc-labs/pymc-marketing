8bb7f5f8b882d8766470ead24793c013b31d0f2a
Will Dean <wd60622@gmail.com>
2025-06-16 11:31:43 -0400

    separate id mismatch from version mismatch

--- pymc_marketing/model_builder.py
+++ pymc_marketing/model_builder.py
@@ -567,7 +567,17 @@ class ModelBuilder(ABC):
         model.build_from_idata(idata)
         model.post_sample_model_transformation()
 
-        if model.id != idata.attrs["id"]:
+        if (model_version := model.version) != (
+            loaded_version := idata.attrs["version"]
+        ):
+            msg = (
+                f"The model version ({loaded_version}) in the InferenceData does not "
+                f"match the model version ({model_version}). "
+                "There was no error loading the inference data, but the model structure "
+                "is different. "
+            )
+            raise DifferentModelError(msg)
+        elif model.id != idata.attrs["id"]:
             msg = (
                 "The model id in the InferenceData does not match the model id. "
                 "There was no error loading the inference data, but the model may "
