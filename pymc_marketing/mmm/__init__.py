0becca4e2d8619e20f503cad59bbbadd2efeaa3b
PabloRoque <pablo.deroque@glovoapp.com>
2025-10-01 15:45:53 +0200

    Add HillShapeSaturation to __init__

--- pymc_marketing/mmm/__init__.py
+++ pymc_marketing/mmm/__init__.py
@@ -27,6 +27,7 @@ from pymc_marketing.mmm.components.adstock import (
 from pymc_marketing.mmm.components.saturation import (
     HillSaturation,
     HillSaturationSigmoid,
+    HillShapeSaturation,
     InverseScaledLogisticSaturation,
     LogisticSaturation,
     LogSaturation,
@@ -96,6 +97,7 @@ __all__ = [
     "HSGPPeriodic",
     "HillSaturation",
     "HillSaturationSigmoid",
+    "HillShapeSaturation",
     "InverseScaledLogisticSaturation",
     "LinearTrend",
     "LogSaturation",
