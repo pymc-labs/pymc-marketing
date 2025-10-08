4881e95e51bce0d4dc4f880c1df4c5374ffec32a
Oriol (ProDesk) <oriol.abril.pla@gmail.com>
2025-10-09 00:00:04 +0200

    add css hotfix for notebook image deformation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -158,6 +158,7 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static/"]
+html_css_files = ["custom.css"]
 
 # -- Options for LaTeX output ---------------------------------------------
 
