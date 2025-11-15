444e07e5bbae092ed90dad33c0ee698d65c21e72
daniel-saunders-phil <dsaunders406@gmail.com>
2025-11-15 13:13:01 -0800

    add a base class to collect shared functionality for Laplace and LogNormal.

--- pymc_marketing/special_priors.py
+++ pymc_marketing/special_priors.py
@@ -21,6 +21,7 @@ of the same methods.
 """
 
 import warnings
+from abc import ABC, abstractmethod
 from typing import Any
 
 import numpy as np
@@ -32,7 +33,111 @@ from pymc_extras.prior import Prior, VariableFactory, create_dim_handler, sample
 from pytensor.tensor import TensorVariable
 
 
-class LogNormalPrior:
+class SpecialPrior(ABC):
+    """A base class for specialized priors."""
+
+    def __init__(self, dims: tuple | None = None, centered: bool = True, **parameters):
+        self.dims = dims
+        self.centered = centered
+        self.parameters = parameters
+        self._checks()
+
+    @abstractmethod
+    def _checks(self) -> None:
+        """Check that the parameters are correct."""
+        pass
+
+    @abstractmethod
+    def create_variable(self, name: str) -> TensorVariable:
+        """Create a variable from the prior distribution."""
+        pass
+
+    def _create_parameter(self, param, value, name):
+        if not hasattr(value, "create_variable"):
+            return value
+
+        child_name = f"{name}_{param}"
+        return self.dim_handler(value.create_variable(child_name), value.dims)
+
+    def to_dict(self):
+        """Convert the SpecialPrior to a dictionary."""
+        class_name = self.__class__.__name__
+        data = {
+            "special_prior": class_name,
+        }
+        if self.parameters:
+
+            def handle_value(value):
+                if isinstance(value, Prior):
+                    return value.to_dict()
+
+                if isinstance(value, pt.TensorVariable):
+                    value = value.eval()
+
+                if isinstance(value, np.ndarray):
+                    return value.tolist()
+
+                if hasattr(value, "to_dict"):
+                    return value.to_dict()
+
+                return value
+
+            data["kwargs"] = {
+                param: handle_value(value) for param, value in self.parameters.items()
+            }
+        if not self.centered:
+            data["centered"] = False
+
+        if self.dims:
+            data["dims"] = self.dims
+
+        return data
+
+    @classmethod
+    def from_dict(cls, data) -> "SpecialPrior":
+        """Create a SpecialPrior prior from a dictionary."""
+        if not isinstance(data, dict):
+            msg = (
+                "Must be a dictionary representation of a prior distribution. "
+                f"Not of type: {type(data)}"
+            )
+            raise ValueError(msg)
+
+        kwargs = data.get("kwargs", {})
+
+        def handle_value(value):
+            if isinstance(value, dict):
+                return deserialize(value)
+
+            if isinstance(value, list):
+                return np.array(value)
+
+            return value
+
+        kwargs = {param: handle_value(value) for param, value in kwargs.items()}
+        centered = data.get("centered", True)
+        dims = data.get("dims")
+        if isinstance(dims, list):
+            dims = tuple(dims)
+
+        return cls(dims=dims, centered=centered, **kwargs)
+
+    def sample_prior(
+        self,
+        coords=None,
+        name: str = "variable",
+        **sample_prior_predictive_kwargs,
+    ) -> xr.Dataset:
+        """Sample from the prior distribution."""
+        return sample_prior(
+            factory=self,
+            coords=coords,
+            name=name,
+            **sample_prior_predictive_kwargs,
+        )
+
+
+class LogNormalPrior(SpecialPrior):
     r"""Lognormal prior parameterized by positive-scale mean and std.
 
     A lognormal prior parameterized by mean and standard deviation
@@ -118,13 +223,6 @@ class LogNormalPrior:
         if set(self.parameters.keys()) != {"mean", "std"}:
             raise ValueError("Parameters must be mean and std")
 
-    def _create_parameter(self, param, value, name):
-        if not hasattr(value, "create_variable"):
-            return value
-
-        child_name = f"{name}_{param}"
-        return self.dim_handler(value.create_variable(child_name), value.dims)
-
     def create_variable(self, name: str) -> TensorVariable:
         """Create a variable from the prior distribution."""
         self.dim_handler = create_dim_handler(self.dims)
@@ -156,82 +254,6 @@ class LogNormalPrior:
 
         return phi
 
-    def to_dict(self):
-        """Convert the prior class to a dictionary."""
-        data = {
-            "special_prior": "LogNormalPrior",
-        }
-        if self.parameters:
-
-            def handle_value(value):
-                if isinstance(value, Prior):
-                    return value.to_dict()
-
-                if isinstance(value, pt.TensorVariable):
-                    value = value.eval()
-
-                if isinstance(value, np.ndarray):
-                    return value.tolist()
-
-                if hasattr(value, "to_dict"):
-                    return value.to_dict()
-
-                return value
-
-            data["kwargs"] = {
-                param: handle_value(value) for param, value in self.parameters.items()
-            }
-        if not self.centered:
-            data["centered"] = False
-
-        if self.dims:
-            data["dims"] = self.dims
-
-        return data
-
-    @classmethod
-    def from_dict(cls, data) -> "LogNormalPrior":
-        """Create a LogNormalPrior prior from a dictionary."""
-        if not isinstance(data, dict):
-            msg = (
-                "Must be a dictionary representation of a prior distribution. "
-                f"Not of type: {type(data)}"
-            )
-            raise ValueError(msg)
-
-        kwargs = data.get("kwargs", {})
-
-        def handle_value(value):
-            if isinstance(value, dict):
-                return deserialize(value)
-
-            if isinstance(value, list):
-                return np.array(value)
-
-            return value
-
-        kwargs = {param: handle_value(value) for param, value in kwargs.items()}
-        centered = data.get("centered", True)
-        dims = data.get("dims")
-        if isinstance(dims, list):
-            dims = tuple(dims)
-
-        return cls(dims=dims, centered=centered, **kwargs)
-
-    def sample_prior(
-        self,
-        coords=None,
-        name: str = "variable",
-        **sample_prior_predictive_kwargs,
-    ) -> xr.Dataset:
-        """Sample from the prior distribution."""
-        return sample_prior(
-            factory=self,
-            coords=coords,
-            name=name,
-            **sample_prior_predictive_kwargs,
-        )
-
 
 def _is_LogNormalPrior_type(data: dict) -> bool:
     if "special_prior" in data:
@@ -246,7 +268,7 @@ register_deserialization(
 )
 
 
-class LaplacePrior:
+class LaplacePrior(SpecialPrior):
     """A Laplace prior parameterized by a location and a scale parameter.
 
     Unlike the standard Laplace distribution available through the Prior class,
@@ -285,13 +307,6 @@ class LaplacePrior:
         if set(self.parameters.keys()) != {"mu", "b"}:
             raise ValueError("Parameters must be mu and b")
 
-    def _create_parameter(self, param, value, name):
-        if not hasattr(value, "create_variable"):
-            return value
-
-        child_name = f"{name}_{param}"
-        return self.dim_handler(value.create_variable(child_name), value.dims)
-
     def create_variable(self, name: str) -> TensorVariable:
         """Create a variable from the prior distribution."""
         self.dim_handler = create_dim_handler(self.dims)
@@ -312,82 +327,6 @@ class LaplacePrior:
             )
 
         return phi
-
-    def to_dict(self):
-        """Conver the prior class to a dictionary."""
-        data = {
-            "special_prior": "LaplacePrior",
-        }
-        if self.parameters:
-
-            def handle_value(value):
-                if isinstance(value, Prior):
-                    return value.to_dict()
-
-                if isinstance(value, pt.TensorVariable):
-                    value = value.eval()
-
-                if isinstance(value, np.ndarray):
-                    return value.tolist()
-
-                if hasattr(value, "to_dict"):
-                    return value.to_dict()
-
-                return value
-
-            data["kwargs"] = {
-                param: handle_value(value) for param, value in self.parameters.items()
-            }
-        if not self.centered:
-            data["centered"] = False
-
-        if self.dims:
-            data["dims"] = self.dims
-
-        return data
-
-    @classmethod
-    def from_dict(cls, data) -> "LaplacePrior":
-        """Create a LaplacePrior prior from a dictionary."""
-        if not isinstance(data, dict):
-            msg = (
-                "Must be a dictionary representation of a prior distribution. "
-                f"Not of type: {type(data)}"
-            )
-            raise ValueError(msg)
-
-        kwargs = data.get("kwargs", {})
-
-        def handle_value(value):
-            if isinstance(value, dict):
-                return deserialize(value)
-
-            if isinstance(value, list):
-                return np.array(value)
-
-            return value
-
-        kwargs = {param: handle_value(value) for param, value in kwargs.items()}
-        centered = data.get("centered", True)
-        dims = data.get("dims")
-        if isinstance(dims, list):
-            dims = tuple(dims)
-
-        return cls(dims=dims, centered=centered, **kwargs)
-
-    def sample_prior(
-        self,
-        coords=None,
-        name: str = "variable",
-        **sample_prior_predictive_kwargs,
-    ) -> xr.Dataset:
-        """Sample from the prior distribution."""
-        return sample_prior(
-            factory=self,
-            coords=coords,
-            name=name,
-            **sample_prior_predictive_kwargs,
-        )
 
 
 def _is_LaplacePrior_type(data: dict) -> bool:
