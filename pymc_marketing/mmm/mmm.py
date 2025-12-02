3212b91e594e5e9d701086777672822b33fd158d
juanitorduz <juanitorduz@gmail.com>
2025-12-02 20:30:39 +0100

    training optimization plots

--- pymc_marketing/mmm/mmm.py
+++ pymc_marketing/mmm/mmm.py
@@ -2406,9 +2406,9 @@ class MMM(
         DataArray
             Samples in original scale
         """
-        if var_names is not None:
-            for var_name in (v for v in var_names if v in samples):
-                samples[var_name] *= self.target_scale
+        vars_to_transform = var_names if var_names is not None else [self.output_var]
+        for var_name in (v for v in vars_to_transform if v in samples):
+            samples[var_name] *= self.target_scale
         return samples
 
     def _transform_to_original_scale_legacy(
