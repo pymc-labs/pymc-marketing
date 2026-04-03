15245d0f8f7ab86a16b216eb556716164f23a61e
tim mcwilliams <tmcwilliams1127@gmail.com>
2026-04-03 14:44:08 -0400

    mathjax config - ensure amsmath package is loaded

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,6 +96,12 @@ nb_kernel_rgx_aliases = {".*": "python3"}
 myst_enable_extensions = ["colon_fence", "deflist", "dollarmath", "amsmath"]
 myst_heading_anchors = 0
 
+# mathjax config - ensure amsmath package is loaded
+mathjax3_config = {
+    "loader": {"load": ["[tex]/amsmath"]},
+    "tex": {"packages": {"[+]": ["amsmath"]}},
+}
+
 # numpydoc and autodoc typehints config
 numpydoc_show_class_members = False
 numpydoc_xref_param_type = True
