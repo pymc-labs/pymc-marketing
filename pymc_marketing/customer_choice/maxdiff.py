f9c28f2346fee510702e78337534349a74195977
Will Dean <wd60622@gmail.com>
2026-06-26 11:40:34 -0400

    chore: remove dead catch_warnings blocks for add_groups
    - The 'fit_data not defined in InferenceData scheme' warning was
      emitted by arviz add_groups, which has been replaced by
      idata["/fit_data"] = fit_data. DataTree assignment never raises it.
    - Removed dead with/catch_warnings/filterwarnings blocks in
      mixed_logit, mnl_logit, nested_logit, maxdiff.

--- pymc_marketing/customer_choice/maxdiff.py
+++ pymc_marketing/customer_choice/maxdiff.py
@@ -1194,13 +1194,7 @@ class MaxDiffMixedLogit(ModelBuilder):
             self.idata = self.idata.drop_nodes("fit_data")
 
         fit_data = self._create_fit_data()
-        with warnings.catch_warnings():
-            warnings.filterwarnings(
-                "ignore",
-                category=UserWarning,
-                message="The group fit_data is not defined in the InferenceData scheme",
-            )
-            self.idata["/fit_data"] = fit_data
+        self.idata["/fit_data"] = fit_data
 
         self.set_idata_attrs(self.idata)
         return self.idata
