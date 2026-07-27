0a25142b1d13d0ea8b8216e27b389dd98d8cf3db
Will Dean <wd60622@gmail.com>
2026-07-27 11:51:40 -0400

    feat(mmm): add DataVarMuEffect, MediaMuEffect, ControlMuEffect for xarray data referencing
    Three new MuEffect subclasses enabling media and control effects that
    read data directly from named variables in the training xr.Dataset.
    
    - DataVarMuEffect: abstract base with create_data/set_data defaults
    - MediaMuEffect: applies MediaTransformation to a named variable,
      aggregates over configurable channel_dim
    - ControlMuEffect: configurable prior per control variable, auto-sums
      extra dimensions
    
    Also extends the Model protocol with xarray_dataset property so all
    MuEffects can access the canonical training data.

--- pymc_marketing/customer_choice/mv_its.py
+++ pymc_marketing/customer_choice/mv_its.py
@@ -22,6 +22,7 @@ import matplotlib.ticker as mtick
 import numpy as np
 import pandas as pd
 import pymc as pm
+import xarray as xr
 from matplotlib.axes import Axes
 from pymc_extras.prior import Prior
 from pytensor.tensor import as_tensor
@@ -74,6 +75,7 @@ class MVITS(RegressionModelBuilder):
         self.mu_effects: list[MuEffect] = []
         # extras dims for the likelihood
         self.dims = ("existing_product",)
+        self.xarray_dataset: xr.Dataset = xr.Dataset()
 
         super().__init__(model_config=model_config, sampler_config=sampler_config)
 
